7720fe0cbe9662c506f76af299eed23d94a8f17c
vlad-gurbanov <vlad-gurbanov@mail.ru>
2023-02-21 15:24:42 +0300

    fix

--- app/config.py
+++
@@ -1,9 +0,0 @@
-# config.py
-
-# Проводим конфигурацию БД. Создаем наши настройки.
-
-class Config:
-    DEBUG = True
-    SECRET = 'test'   # в конфиг выносят некие секретные данные(пример)
-    SQLALCHEMY_DATABASE_URI = 'sqlite:///:memory:'
-    SQLALCHEMY_TRACK_MODIFICATIONS = False
--- app/database.py
+++
@@ -1,7 +0,0 @@
-# database.py
-
-# Содержит создание объекта db вместе с импортом.
-from flask_sqlalchemy import SQLAlchemy
-
-# Создаем соединение с базой данных.
-db = SQLAlchemy()
